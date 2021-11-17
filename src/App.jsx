dfc2e56492f94867035c4622e457b8a8aaa1f686
Erick Alonso <alcuadradoalonso@gmail.com>
2021-11-17 09:16:32 -0600

    Initial configuration and bootstrap done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Componente principal</h1>
+      <button className='btn btn-primary'>Click me</button>
+    </div>
+  );
+}
+
+export default App;
