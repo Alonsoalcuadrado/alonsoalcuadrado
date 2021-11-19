30fa95ca9383eb21574fd3b62acfed07e05dbf8f
ManuelZamora <rocker2503@gmail.com>
2021-11-19 14:46:44 -0600

    add Navbar to project

---
+++ src/components/Navbar/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from './../../assets/logo.png'
+import './NavBar.scss'
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+function NavBar() {
+  return (
+    <Navbar id="nav" expand="lg" className="navbar" variant="dark">
+      <Container>
+        <img src={logo} alt="logo" className="Nav-logo" width="45" height="35"/>
+        <Navbar.Brand href="#home">
+          Al Cuadrado</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse id="basic-navbar-nav" className="d-flex justify-content-end">
+          <Nav>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#servicios">Servicios</Nav.Link>
+            <Nav.Link href="#aboutus">Nosotros</Nav.Link>
+            <Nav.Link href="#contact">Contacto</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
