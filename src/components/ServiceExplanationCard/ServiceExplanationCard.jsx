25cc77f8d47aaca7f005b4e5a0c3fd681634c933
ximena <ximenayazmiin@gmail.com>
2021-11-19 12:03:15 -0600

    it was deleted console.log

---
+++ src/components/ServiceExplanationCard/ServiceExplanationCard.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import './ServiceExplanationCardsStyle.scss'
+import { Col,Row } from 'react-bootstrap'
+
+
+function ServiceExplanationCard({serviceCardTitle,serviceCardPoints,serviceCardId,serviceCardImge}) {
+    if((serviceCardId%2)===0){
+        return (
+        
+            <div className="card-information ">
+               <div className="card-information__card--aling-left">
+                  <Row>
+                      <Col> <img className="card-information__img" src={serviceCardImge} alt="Not found"></img></Col>
+                      <Col> 
+                        <p className="card-information__title">{serviceCardTitle}</p>
+                            <ul className="card-information__points">
+                                {
+                                    serviceCardPoints.map(serviceCardPoint=>(
+                                        <li key={serviceCardPoint.id}>{serviceCardPoint.p}</li>
+                                    ))
+                                }
+                            </ul>
+                      </Col>
+                  </Row>
+               </div>
+            </div>
+    )
+    }
+    else{
+        return (       
+        
+            <div className="card-information">
+               <div className="card-information__card--aling-right">
+                  <Row >
+                      <Col> <img className="card-information__img" src={serviceCardImge} alt="Not found"></img></Col>
+                      <Col> 
+                        <p className="card-information__title">{serviceCardTitle}</p>
+                            <ul className="card-information__points">
+                                {
+                                    serviceCardPoints.map(serviceCardPoint=>(
+                                        <li key={serviceCardPoint.id}> {serviceCardPoint.p}</li>
+        
+                                    ))
+                                }
+                            </ul>
+                      </Col>
+                  </Row>
+               </div>
+            </div>
+        )
+    }
+}
+export default ServiceExplanationCard
--- src/components/benefitsSpace/benefitsCards.jsx
+++ src/components/benefitsSpace/benefitsCards.jsx
@@ -2,17 +2,14 @@ import React from 'react'
 import './benefitsStyle.scss'
 
 function benefitsCardsRight({infoCard}) {
-    const infoCard2 = {...infoCard}
-    console.log({infoCard2})
-
-    if((infoCard2.id%2)===0){
+    if((infoCard.id%2)===0){
         return(
             <>    
                     <div className="cardBig col ">
-                        <p className="p-title">{infoCard2.title}</p>
-                        <p>{infoCard2.text}</p>
+                        <p className="p-title">{infoCard.title}</p>
+                        <p>{infoCard.text}</p>
                     </div>
-                    <div key={infoCard2.id}  className="cardSmall col-2 "><img src={infoCard2.icono} alt="Not found"></img></div>
+                    <div key={infoCard.id}  className="cardSmall col-2 "><img src={infoCard.icono} alt="Not found"></img></div>
          </>
         
         )
@@ -20,10 +17,10 @@ function benefitsCardsRight({infoCard}) {
       else{
         return(
             <>
-            <div key={infoCard2.id}  className="cardSmall col-2 "><img src={infoCard2.icono} alt="Not found"></img></div>
+            <div key={infoCard.id}  className="cardSmall col-2 "><img src={infoCard.icono} alt="Not found"></img></div>
             <div className="cardBig col ">
-                <p className="p-title">{infoCard2.title}</p>
-                <p>{infoCard2.text}</p>
+                <p className="p-title">{infoCard.title}</p>
+                <p>{infoCard.text}</p>
             </div>
         </>
 
